411b59185fc80c7080c48c9e9e1c7de14ce81095
wangshuaifei <wangshuaifei@piesat.cn>
2019-12-27 11:14:26 +0800

    wsf

--- src/components/legend/demo/portrait/index.jsx
+++ src/components/legend/demo/portrait/index.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 
-import Legend from '../index'
+import Legend from '../index';
 
 const dataSource = {
   isHorizontal: false,
-  isLableOnLine: false,
+  isLableOnLine: true,
+  isGradient: true,
   title: '温度（℃）',
-  value: [1, 2, 3, 4,5],
+  value: [1, 2, 3, 4],
   valueMaxLength: 1,
-  color: ['rgba(200,200,200,1)', 'rgba(190,190,190,1)', 'rgba(130,130,130,1)', 'rgba(60,60,60,1)', 'rgba(0,0,0,1)'],
+  color: [
+    'rgba(200,200,200,1)',
+    'rgba(190,190,190,1)',
+    'rgba(130,130,130,1)',
+    'rgba(60,60,60,1)',
+    'rgba(0,0,0,1)',
+    'rgba(0,0,0,1)'],
 };
 
 function LegendPortrait() {
-  return <Legend dataSource={dataSource}/>
+  return <Legend dataSource={dataSource}/>;
 }
 
-export default LegendPortrait
+export default LegendPortrait;
